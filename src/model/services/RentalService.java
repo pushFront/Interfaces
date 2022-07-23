8d62fe38935447d73817e0302dd0ed2188e39149
Rian Lima <rlimamendes@proton.me>
2022-07-22 23:13:24 -0300

    Solution without interface

---
+++ src/model/services/RentalService.java
@@ -0,0 +1,61 @@
+package model.services;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+import java.util.concurrent.TimeUnit;
+
+public class RentalService {
+    private Double pricePerDay;
+    private Double pricePerHour;
+
+    private BrazilTaxService taxService;
+
+    public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+        this.pricePerDay = pricePerDay;
+        this.pricePerHour = pricePerHour;
+        this.taxService = taxService;
+    }
+
+    public Double getPricePerDay() {
+        return pricePerDay;
+    }
+
+    public void setPricePerDay(Double pricePerDay) {
+        this.pricePerDay = pricePerDay;
+    }
+
+    public Double getPricePerHour() {
+        return pricePerHour;
+    }
+
+    public void setPricePerHour(Double pricePerHour) {
+        this.pricePerHour = pricePerHour;
+    }
+
+    public BrazilTaxService getTaxService() {
+        return taxService;
+    }
+
+    public void setTaxService(BrazilTaxService taxService) {
+        this.taxService = taxService;
+    }
+
+    public void processInvoice(CarRental carRental) {
+        long t1 = carRental.getFinish().getTime();
+        long t2 = carRental.getStart().getTime();
+        double hours = (double) (t1 - t2) / 1000 / 60 / 60;
+
+        double basicPayment;
+        if(hours <= 12.00) {
+            basicPayment = Math.ceil(hours) * pricePerHour;
+        }
+        else {
+            basicPayment = Math.ceil(hours / 24) * pricePerDay;
+        }
+
+        double tax = taxService.tax(basicPayment);
+
+        carRental.setInvoice(new Invoice(basicPayment, tax));
+    }
+}
